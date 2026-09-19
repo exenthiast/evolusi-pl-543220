366b56bd8fc281a52e3383e0366ea59ead14da96
Alfiz Desta <pizzparker@Alfiz-MacBook-Air.local>
2026-09-19 19:36:19 +0700

    test: make ExampleTest fail intentionally for scenario B

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,8 @@
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
+    // Sengaja dibuat gagal untuk pengujian Skenario B
+    $this->assertTrue(false);
+
     $response->assertStatus(200);
 });
